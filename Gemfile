be3c3d94d35117c555e76887da4888f7668e929a
souiti nakajima <nakajimasouiti@gmail.com>
2018-09-18 16:43:07 +0000

    ranking

--- Gemfile
+++ Gemfile
@@ -54,3 +54,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'rakuten_web_service' , '1.5.0'
 gem 'dotenv-rails'
+gem 'pry-byebug', group: :development
--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -1,5 +1,6 @@
 class OwnershipsController < ApplicationController
  def create
+   
     @item = Item.find_or_initialize_by(code: params[:item_code])
 
     unless @item.persisted?
@@ -14,6 +15,9 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
+    elsif params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'
     end
 
     redirect_back(fallback_location: root_path)
@@ -25,7 +29,11 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
+    elsif params[:type] == 'Have'
+      current_user.unhave(@item) 
+      flash[:success] = '商品の Have を解除しました。'
     end
+
 
     redirect_back(fallback_location: root_path)
   end
